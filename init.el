37cf649fc2dcf455a8ebb0b1e3e6e1c43b42c421
h-nakajm <h-nakajm@ist.osaka-u.ac.jp>
2016-08-28 17:14:23 +0900

    modified:   init.el 	marmalade removed

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
  (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
 ;; Marmaladeを追加
- (add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;; (add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
 ;; Orgを追加
  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
