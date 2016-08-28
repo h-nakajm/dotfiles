92c692814853c7b9391695d5e62c41d5114ad64e
h-nakajm <h-nakajm@ist.osaka-u.ac.jp>
2016-08-28 16:47:52 +0900

    new file:   init.el 	emacs configuration file

---
+++ init.el
@@ -0,0 +1,150 @@
+;; load-pathの追加関数
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+
+;; load-pathに追加するフォルダ
+;; 2つ以上フォルダを指定する場合の引数 => (add-to-load-path "elisp" "xxx" "xxx")
+(add-to-load-path "elisp")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'package)
+
+;; MELPAを追加
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+
+;; MELPA-stableを追加
+ (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+;; Marmaladeを追加
+ (add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+;; Orgを追加
+ (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+;; 初期化
+ (package-initialize)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; スタートアップメッセージを表示させない
+(setq inhibit-startup-message t)
+
+;; フォント設定
+(add-to-list 'default-frame-alist '(font . "Myrica M"))
+
+;; テーマの設定
+;;(load-theme 'deeper-blue' t)
+
+;; Emacsのカラーテーマ
+;; http://code.google.com/p/gnuemacscolorthemetest/
+(when (and (require 'color-theme nil t) (window-system))
+  (color-theme-initialize)
+  (color-theme-clarity))
+
+;; Ctrl+hでBackSpace
+(keyboard-translate ?\C-h ?\C-?)
+
+;; 列数を表示する
+(column-number-mode t)
+
+;; 行数を表示する
+(global-linum-mode t)
+
+;; 改行コードを表示する
+(setq eol-mnemonic-dos "(CRLF)")
+(setq eol-mnemonic-mac "(CR)")
+(setq eol-mnemonic-unix "(LF)")
+
+;; スペース、タブなどを可視化する
+;; (global-whitespace-mode 1)
+
+;; タブ幅
+(custom-set-variables '(tab-width 4))
+
+;; 対応する括弧を光らせる
+(show-paren-mode 1)
+
+;; バックアップファイルを作成させない
+(setq make-backup-files nil)
+
+;; 終了時にオートセーブファイルを削除する
+(setq delete-auto-save-files t)
+
+;; beep音を消す
+(defun my-bell-function ()
+  (unless (memq this-command
+        '(isearch-abort abort-recursive-edit exit-minibuffer
+              keyboard-quit mwheel-scroll down up next-line previous-line
+              backward-char forward-char))
+    (ding)))
+(setq ring-bell-function 'my-bell-function)
+
+;; for markdown
+(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;; 1行ずつスクロール
+(setq scroll-conservatively 35
+      scroll-margin 0
+      scroll-step 1)
+(setq comint-scroll-show-maximum-output t) ;; shell-mode
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; タブ化
+;; http://www.emacswiki.org/emacs/tabbar.el
+;;(require 'cl)
+
+(when (require 'tabbar nil t)
+  (tabbar-mode))
+
+;; ボタンをシンプルにする
+(setq tabbar-home-button-enabled "")
+(setq tabbar-scroll-right-button-enabled "")
+(setq tabbar-scroll-left-button-enabled "")
+(setq tabbar-scroll-right-button-disabled "")
+(setq tabbar-scroll-left-button-disabled "")
+
+;; Ctrl-Tab, Ctrl-Shift-Tab でタブを切り替える
+(dolist (func '(tabbar-mode tabbar-forward-tab tabbar-forward-group tabbar-backward-tab tabbar-backward-group))
+  (autoload func "tabbar" "Tabs at the top of buffers and easy control-tab navigation"))
+(defmacro defun-prefix-alt (name on-no-prefix on-prefix &optional do-always)
+  `(defun ,name (arg)
+     (interactive "P")
+     ,do-always
+     (if (equal nil arg)
+         ,on-no-prefix
+       ,on-prefix)))
+(defun-prefix-alt shk-tabbar-next (tabbar-forward-tab) (tabbar-forward-group) (tabbar-mode 1))
+(defun-prefix-alt shk-tabbar-prev (tabbar-backward-tab) (tabbar-backward-group) (tabbar-mode 1))
+(global-set-key [(control tab)] 'shk-tabbar-next)
+(global-set-key [(control shift tab)] 'shk-tabbar-prev)
+
+;; GUIで直接ファイルを開いた場合フレームを作成しない
+(add-hook 'before-make-frame-hook
+          (lambda ()
+            (when (eq tabbar-mode t)
+              (switch-to-buffer (buffer-name))
+              (delete-this-frame))))
+
+;; 例: 全バッファを一つのグループにしまう
+(setq tabbar-buffer-groups-function (lambda () (list "Buffers")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; for javascript
+(require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+;;auto-completeの設定
+(require 'auto-complete)
+(global-auto-complete-mode t)
+(setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
+(setq ac-use-fuzzy t)          ;; 曖昧マッチ
+
