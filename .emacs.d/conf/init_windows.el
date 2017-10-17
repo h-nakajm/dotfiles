53198de4e63bca92eb4925a03d27dd8224804021
h-nakajm <h-nakajm@ist.osaka-u.ac.jp>
2017-10-17 19:11:12 +0900

    devide init.el to .emacs.d/conf/init_windows.el

---
+++ .emacs.d/conf/init_windows.el
@@ -0,0 +1,7 @@
+;; superキーの設定
+;; 左WindowsキーをSuperに設定
+(setq w32-pass-lwindow-to-system nil)
+(setq w32-lwindow-modifier 'super)
+
+;; C-s-jで行番号ジャンプ
+(define-key global-map (kbd "C-s-j") 'goto-line)
